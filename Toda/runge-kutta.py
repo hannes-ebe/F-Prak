7aff8d10360b651b83184731ea048183fb05b05c
eiket2000 <eiket2000@gmail.com>
2023-02-04 14:06:04 +0100

    implemented coupled runge kutta and python interface

--- Toda/runge-kutta.py
+++ Toda/runge-kutta.py
@@ -1,5 +1,26 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import os
 
-Q0=1
-i0=1
+print("initialize calculation")
+Q0=0
+I0=0
+omega=1
+U_pp=2
+L=1
+R=100
+np.savetxt("start.txt",[Q0,I0])
+print("Start calculation")
+os.system("ifort runge.f90 -O -o runge-fortran.sh")
+os.system("./runge-fortran.sh")
+print("finished")
 
+t=np.loadtxt("result_time.txt")
+Q=np.loadtxt("result_Q.txt")
+I=np.loadtxt("result_I.txt")
+Q_ref=[t for t in t]
+plt.plot(t,Q,label="numerik")
+plt.plot(t,I,label="numerik")
+#plt.plot(t,Q_ref,label="Theorie")
+plt.legend()
+plt.show()
--- Toda/runge.f90
+++ Toda/runge.f90
@@ -1,32 +1,61 @@
 program runge_kutta
     implicit none
-    ! This is a comment line; it is ignored by the compiler
-    real, dimension(10d1) :: Q, I
-    real :: dt,t
+    !declare precision
+    integer, parameter :: ikind=selected_real_kind(p=20)
+    ! declare variables
+    real(kind=ikind), dimension(2d3) :: Q, I, Tl
+    real(kind=ikind) :: dt,t,Q0, I0
     integer :: ci, steps
+    !setup start varibles
     steps = SIZE(Q)-1
-    dt=0.1
-    Q(0) = 0
-    I(0) = 0
-    do ci = 0,steps
+    dt=0.001
+    open(10,file="start.txt")
+    read(10,*)  Q0,I0
+    Q(1) = Q0
+    I(1) = I0
+    Tl(1) = 0
+    !iterate over time and solve runge kutta
+    do ci = 1,steps
         t=dt*ci
+        Tl(ci+1)=t
         call runge_kutta_step(t,dt,Q(ci),I(ci),Q(ci+1),I(ci+1))
-        print *, ci,t,Q(ci),Q(ci+1)
     end do
+    !Save results
+    open(20,file="result_time.txt")
+    write(20,*) Tl
+    open(30,file="result_Q.txt")
+    write(30,*) Q
+    open(40,file="result_I.txt")
+    write(40,*) I
+    print *, "test"
 end program runge_kutta
 
 subroutine runge_kutta_step(t,dt,Q,I,Q_new,I_new)
-    real :: Q,I,Q_new,I_new,k1,k2,k3,k4
-    real :: t,dt
-    k1=f(t,Q)
-    k2=f(t+ dt/2,Q+dt/2*k1)
-    k3=f(t+ dt/2,Q+dt/2*k2)
-    k4=f(t+ dt,Q+h*k3)
-    Q_new=Q+(k1+2*k2+2*k3+k4)*dt*1/6
+    integer, parameter :: ikind=selected_real_kind(p=20)
+    real(kind=ikind) :: Q,I,Q_new,I_new,Qk1,Qk2,Qk3,Qk4,Ik1,Ik2,Ik3,Ik4
+    real(kind=ikind) :: t,dt
+    !calculate Qks
+    Qk1=fQ(t,Q,I)
+    Qk2=fQ(t+ dt/2,Q+dt/2*Qk1,I+dt/2*Qk1)
+    Qk3=fQ(t+ dt/2,Q+dt/2*Qk2,I+dt/2*Qk2)
+    Qk4=fQ(t+ dt,Q+dt*Qk3,I+dt*Qk3)
+    !calculate Iks
+    Ik1=fI(t,Q,I)
+    Ik2=fI(t+ dt/2,Q+dt/2*Ik1,I+dt/2*Ik1)
+    Ik3=fI(t+ dt/2,Q+dt/2*Ik2,I+dt/2*Ik2)
+    Ik4=fI(t+ dt,Q+dt*Ik3,I+dt*Ik3)
+    !set new values
+    Q_new=Q+(Qk1+2*Qk2+2*Qk3+Qk4)*dt*1/6
+    I_new=I+(Ik1+2*Ik2+2*Ik3+Ik4)*dt*1/6
     end subroutine
 
-function f(x,u)
-    real :: x,u
-    f=x
+function fQ(x,q,i)
+    integer, parameter :: ikind=selected_real_kind(p=20)
+    real(kind=ikind) :: x,q,i
+    fQ=x
    end function
-
+function fI(x,q,i)
+    integer, parameter :: ikind=selected_real_kind(p=20)
+    real(kind=ikind) :: x,q,i
+    fI=2*x
+   end function
